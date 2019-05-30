9c0cf27815177aa627f0584182aa33947f3eb48d
Philip Schmidt <philip.schmidt@ibm.com>
2019-05-30 16:07:06 -0500

    add ibmcloud aliases and change default theme

---
+++ plugins/ibmcloud/ibmcloud.plugin.zsh
@@ -0,0 +1,18 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+alias ic='ibmcloud'
+
+# ic devops operations
+alias icdo='ic devops'
+alias icdot='icdo toolchain'
+alias icdots='icdo toolchains'
+alias icdod='icdo toolchain-delete'
+alias icdoo='icdo toolchain-open'
+
+# ic dev operations
+alias icd='ic dev'
+alias icdc='icd create --trace'
+
+# ic ks operations
+alias ick='ic ks'
+alias ickc='ick cluster-config'
